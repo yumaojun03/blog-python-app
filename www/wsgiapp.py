a3dd20c43f4ca1bd7a6602fc9c042969a247ae44
zhichuanxiu <719118794@qq.com>
2015-09-18 13:48:46 +0800

    add web frontend page.

--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -31,6 +31,23 @@ import urls
 
 wsgi.add_module(urls)
 
+# jinja2 filter
+import time, datetime
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+template_engine.add_filter('datetime', datetime_filter)
+
 if __name__ == '__main__':
     wsgi.run(port=9000, host='0.0.0.0')
 
